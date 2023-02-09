scaling fix

other_methods/matchit.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Apr 18 15:20:12 2020
 @author: Harsh
@@ -7,12 +6,13 @@ Created on Sat Apr 18 15:20:12 2020
 import pandas as pd
 import numpy as np
 
+import rpy2
 from rpy2.robjects.packages import importr
 from rpy2.robjects.packages import SignatureTranslatedAnonymousPackage
 import rpy2.robjects.numpy2ri
 
 rpy2.robjects.numpy2ri.activate()
-from rpy2.robjects import pandas2ri
+from rpy2.robjects import r, pandas2ri
 
 pandas2ri.activate()
 
@@ -47,14 +47,16 @@ def matchit(outcome, treatment, data, method='nearest', replace=False):
     names2 <- as.numeric(names(r2$match.matrix[,]))
     mtch2 <- data2[as.numeric(names(r2$match.matrix[,])),]
     hh2 <- data2[as.numeric(r2$match.matrix[,]),'%s'] - data2[as.numeric(names(r2$match.matrix[,])),'%s']
-    """ % (formula_cov, method, replace, outcome, outcome, treatment, treatment, formula_cov, method, replace, outcome,
-           outcome)
+    """ % (
+    formula_cov, method, replace, outcome, outcome, treatment, treatment,
+    formula_cov, method, replace, outcome, outcome)
 
     psnn = SignatureTranslatedAnonymousPackage(string, "powerpack")
     match = psnn.mtch
     match2 = psnn.mtch2
     t_hat = pd.DataFrame(np.hstack((np.array(psnn.hh), np.array(psnn.hh2))),
-                         index=list(psnn.names.astype(int)) + list(psnn.names2.astype(int)),
+                         index=list(psnn.names.astype(int)) + list(
+                             psnn.names2.astype(int)),
                          columns=['CATE'])
     ate = np.mean(t_hat['CATE'])
     return ate, t_hat