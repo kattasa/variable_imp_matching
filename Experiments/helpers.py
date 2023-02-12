fixing accuracy scaling script

Experiments/helpers.py
@@ -20,15 +20,19 @@ def create_folder(data, print_progress=True):
     return save_folder
 
 
-def get_acic_data(year, file, n_train):
+def get_acic_data(year, file, n_train, return_ids=False):
     if year == 'acic_2019':
-        df_train, df_data, df_true, x_cols, binary, categorical, dummy_cols, categorical_to_dummy = dgp_acic_2019_df(
-            file, n_train=n_train)
+        df_train, df_data, df_true, x_cols, binary, categorical, dummy_cols, \
+        categorical_to_dummy, sample_ids = dgp_acic_2019_df(file, n_train=n_train)
     elif year == 'acic_2018':
-        df_train, df_data, df_true, x_cols, binary, categorical, dummy_cols, categorical_to_dummy = dgp_acic_2018_df(
-            file, n_train=n_train)
+        df_train, df_data, df_true, x_cols, binary, categorical, dummy_cols, \
+        categorical_to_dummy, sample_ids = dgp_acic_2018_df(file, n_train=n_train)
     if n_train > 0:
+        if return_ids:
+            return df_train, df_data, df_true, binary, categorical, dummy_cols, categorical_to_dummy, sample_ids
         return df_train, df_data, df_true, binary, categorical, dummy_cols, categorical_to_dummy
+    if return_ids:
+        return df_data, df_true, binary, categorical, dummy_cols, categorical_to_dummy, sample_ids
     return df_data, df_true, binary, categorical, dummy_cols, categorical_to_dummy
 
 
@@ -106,3 +110,56 @@ def get_errors(est_cates, true_cates, method_name, scale=None, iter=None):
     if iter is not None:
         cates['Iter'] = iter
     return cates
+
+
+def get_mg_comp(df_orig, sample_num, sample, lcm_mgs, linear_prog_c_mg, linear_prog_t_mg,
+                ensemble_prog_c_mg, ensemble_prog_t_mg, n_iters, treatment, ordinal,
+                k_est, imp_covs):
+    while True:
+        iter_number = np.random.randint(0, n_iters)
+        if sample_num in linear_prog_c_mg[iter_number].index:
+            break
+    print(f'Pulling example MG from iteration {iter_number}')
+
+    lcm_c_mg = df_orig.loc[lcm_mgs[iter_number][0].loc[sample_num], imp_covs + [treatment]]
+    lcm_t_mg = df_orig.loc[lcm_mgs[iter_number][1].loc[sample_num], imp_covs + [treatment]]
+    linear_prog_c_mg = df_orig.loc[linear_prog_c_mg[iter_number].loc[sample_num], imp_covs + [treatment]]
+    linear_prog_t_mg = df_orig.loc[linear_prog_t_mg[iter_number].loc[sample_num], imp_covs + [treatment]]
+    ensemble_prog_c_mg = df_orig.loc[ensemble_prog_c_mg[iter_number].loc[sample_num], imp_covs + [treatment]]
+    ensemble_prog_t_mg = df_orig.loc[ensemble_prog_t_mg[iter_number].loc[sample_num], imp_covs + [treatment]]
+
+    lcm_mg = pd.concat([lcm_c_mg, lcm_t_mg])
+    linear_prog_mg = pd.concat([linear_prog_c_mg, linear_prog_t_mg])
+    ensemble_prog_mg = pd.concat([ensemble_prog_c_mg, ensemble_prog_t_mg])
+
+    categorical = list(lcm_mg.dtypes[lcm_mg.dtypes == 'int'].index)
+    categorical.remove(treatment)
+    for o in ordinal:
+        categorical.remove(o)
+    continuous = list(lcm_mg.dtypes[lcm_mg.dtypes == 'float'].index)
+    continuous = ordinal + continuous
+
+    lcm_comps = {}
+    linear_prog_comps = {}
+    ensemble_prog_comps = {}
+
+    for c in categorical:
+        lcm_comps[c] = ((lcm_mg[c] == sample[c].values[0]).astype(int).sum() / (k_est*2))*100
+        linear_prog_comps[c] = ((linear_prog_mg[c] == sample[c].values[0]).astype(int).sum() / (
+                    k_est * 2)) * 100
+        ensemble_prog_comps[c] = ((ensemble_prog_mg[c] == sample[c].values[0]).astype(int).sum() / (
+                    k_est * 2)) * 100
+    for c in continuous:
+        lcm_comps[c] = np.abs(lcm_mg[c] - sample[c].values[0]).mean()
+        linear_prog_comps[c] = np.abs(linear_prog_mg[c] - sample[c].values[0]).mean()
+        ensemble_prog_comps[c] = np.abs(ensemble_prog_mg[c] - sample[c].values[0]).mean()
+
+    lcm_comps[treatment] = np.nan
+    linear_prog_comps[treatment] = np.nan
+    ensemble_prog_comps[treatment] = np.nan
+
+    lcm_mg = pd.concat([sample, lcm_mg, pd.DataFrame.from_dict([lcm_comps])])
+    linear_prog_mg = pd.concat([sample, linear_prog_mg, pd.DataFrame.from_dict([linear_prog_comps])])
+    ensemble_prog_mg = pd.concat([sample, ensemble_prog_mg, pd.DataFrame.from_dict([ensemble_prog_comps])])
+
+    return lcm_mg, linear_prog_mg, ensemble_prog_mg

Experiments/schools/run.py
@@ -10,6 +10,7 @@ import pickle
 
 from datagen.dgp_df import dgp_schools_df
 
+from Experiments.helpers import get_mg_comp
 from src.linear_coef_matching_mf import LCM_MF
 from other_methods import prognostic
 
@@ -17,7 +18,7 @@ save_folder = os.getenv('SAVE_FOLDER')
 k_est = 10
 random_state = 0
 n_splits = 2
-n_repeats = 50
+n_repeats = 1
 
 df = dgp_schools_df()
 
@@ -200,57 +201,6 @@ for cov, v in Counter(imp_covs2).items():
 print(f'# imp covs: {len(imp_covs)}')
 print(imp_covs)
 
-
-def get_mg_comp(sample_num, sample, lcm_mgs, linear_prog_c_mg, linear_prog_t_mg,
-                ensemble_prog_c_mg, ensemble_prog_t_mg):
-    while True:
-        iter_number = np.random.randint(0, n_splits*n_repeats)
-        if sample_num in linear_prog_c_mg[iter_number].index:
-            break
-    print(f'Pulling example MG from iteration {iter_number}')
-
-    lcm_c_mg = df_orig.loc[lcm_mgs[iter_number][0].loc[sample_num], imp_covs + ['Z']]
-    lcm_t_mg = df_orig.loc[lcm_mgs[iter_number][1].loc[sample_num], imp_covs + ['Z']]
-    linear_prog_c_mg = df_orig.loc[linear_prog_c_mg[iter_number].loc[sample_num], imp_covs + ['Z']]
-    linear_prog_t_mg = df_orig.loc[linear_prog_t_mg[iter_number].loc[sample_num], imp_covs + ['Z']]
-    ensemble_prog_c_mg = df_orig.loc[ensemble_prog_c_mg[iter_number].loc[sample_num], imp_covs + ['Z']]
-    ensemble_prog_t_mg = df_orig.loc[ensemble_prog_t_mg[iter_number].loc[sample_num], imp_covs + ['Z']]
-
-    lcm_mg = pd.concat([lcm_c_mg, lcm_t_mg])
-    linear_prog_mg = pd.concat([linear_prog_c_mg, linear_prog_t_mg])
-    ensemble_prog_mg = pd.concat([ensemble_prog_c_mg, ensemble_prog_t_mg])
-
-    categorical = list(lcm_mg.dtypes[lcm_mg.dtypes == 'int'].index)
-    categorical.remove('Z')
-    categorical.remove('S3')
-    continuous = list(lcm_mg.dtypes[lcm_mg.dtypes == 'float'].index)
-    continuous = ['S3'] + continuous
-
-    lcm_comps = {}
-    linear_prog_comps = {}
-    ensemble_prog_comps = {}
-
-    for c in categorical:
-        lcm_comps[c] = ((lcm_mg[c] == sample[c].values[0]).astype(int).sum() / (k_est*2))*100
-        linear_prog_comps[c] = ((linear_prog_mg[c] == sample[c].values[0]).astype(int).sum() / (
-                    k_est * 2)) * 100
-        ensemble_prog_comps[c] = ((ensemble_prog_mg[c] == sample[c].values[0]).astype(int).sum() / (
-                    k_est * 2)) * 100
-    for c in continuous:
-        lcm_comps[c] = np.abs(lcm_mg[c] - sample[c].values[0]).mean()
-        linear_prog_comps[c] = np.abs(linear_prog_mg[c] - sample[c].values[0]).mean()
-        ensemble_prog_comps[c] = np.abs(ensemble_prog_mg[c] - sample[c].values[0]).mean()
-
-    lcm_comps['Z'] = np.nan
-    linear_prog_comps['Z'] = np.nan
-    ensemble_prog_comps['Z'] = np.nan
-
-    lcm_mg = pd.concat([sample, lcm_mg, pd.DataFrame.from_dict([lcm_comps])])
-    linear_prog_mg = pd.concat([sample, linear_prog_mg, pd.DataFrame.from_dict([linear_prog_comps])])
-    ensemble_prog_mg = pd.concat([sample, ensemble_prog_mg, pd.DataFrame.from_dict([ensemble_prog_comps])])
-
-    return lcm_mg, linear_prog_mg, ensemble_prog_mg
-
 np.random.seed(0)
 sample = df_orig.copy(deep=True)
 for c in imp_covs:
@@ -270,12 +220,17 @@ pickle.dump(linear_prog_t_mg, open(f"{save_folder}/linear_prog_t_mg.pkl", "wb"))
 pickle.dump(ensemble_prog_c_mg, open(f"{save_folder}/ensemble_prog_c_mg.pkl", "wb"))
 pickle.dump(ensemble_prog_t_mg, open(f"{save_folder}/ensemble_prog_t_mg.pkl", "wb"))
 
-lcm_mg, linear_prog_mg, ensemble_prog_mg = get_mg_comp(sample_num, this_sample,
+lcm_mg, linear_prog_mg, ensemble_prog_mg = get_mg_comp(df_orig, sample_num, this_sample,
                                                        lcm.get_MGs(),
                                                        linear_prog_c_mg,
                                                        linear_prog_t_mg,
                                                        ensemble_prog_c_mg,
-                                                       ensemble_prog_t_mg)
+                                                       ensemble_prog_t_mg,
+                                                       n_iters=n_splits*n_repeats,
+                                                       treatment='Z',
+                                                       ordinal=['S3'],
+                                                       k_est=k_est,
+                                                       imp_covs=imp_covs)
 
 lcm_mg = lcm_mg.rename(columns={'Z': 'T'})
 linear_prog_mg = linear_prog_mg.rename(columns={'Z': 'T'})

datagen/dgp_df.py
@@ -176,17 +176,17 @@ def dgp_acic_2019_df(dataset_idx, perc_train=None, n_train=None, dummy_cutoff=10
 
 
 def dgp_acic_2018_df(acic_file, perc_train=None, n_train=None):
-    if os.path.isfile(f'{ACIC_2018_FOLDER}/covariates/x_preprocessed.csv'):
-        df = pd.read_csv(f'{ACIC_2018_FOLDER}/covariates/x_preprocessed.csv').set_index('sample_id')
-        with open(f'{ACIC_2018_FOLDER}/covariates/x_binary.csv') as d:
-            binary = d.read().replace('\n', '').split(',')
-        with open(f'{ACIC_2018_FOLDER}/covariates/x_categorical.csv') as d:
-            categorical = d.read().replace('\n', '').split(',')
-        with open(f'{ACIC_2018_FOLDER}/covariates/x_dummy.csv') as d:
-            dummy_cols = d.read().replace('\n', '').split(',')
-    else:
-        df = pd.read_csv(f'{ACIC_2018_FOLDER}/covariates/x.csv').set_index('sample_id')
-        df, binary, categorical, dummy_cols = clean_2018_covariates(df)
+    # if os.path.isfile(f'{ACIC_2018_FOLDER}/covariates/x_preprocessed.csv'):
+    #     df = pd.read_csv(f'{ACIC_2018_FOLDER}/covariates/x_preprocessed.csv').set_index('sample_id')
+    #     with open(f'{ACIC_2018_FOLDER}/covariates/x_binary.csv') as d:
+    #         binary = d.read().replace('\n', '').split(',')
+    #     with open(f'{ACIC_2018_FOLDER}/covariates/x_categorical.csv') as d:
+    #         categorical = d.read().replace('\n', '').split(',')
+    #     with open(f'{ACIC_2018_FOLDER}/covariates/x_dummy.csv') as d:
+    #         dummy_cols = d.read().replace('\n', '').split(',')
+    # else:
+    df = pd.read_csv(f'{ACIC_2018_FOLDER}/covariates/x.csv').set_index('sample_id')
+    df, binary, categorical, dummy_cols = clean_2018_covariates(df)
     categorical_to_dummy = {}
     for c in categorical:
         categorical_to_dummy[c] = [k for k in dummy_cols if '_'.join(k.split('_')[:-1]) == c]
@@ -212,6 +212,7 @@ def dgp_acic_2018_df(acic_file, perc_train=None, n_train=None):
                                    corr.iloc[list(range(0, i)) +
                                              list(range(i + 1, n)), i] == 1]
                                .index)
+    corr_drop_cols = corr_drop_cols
     df = df.drop(columns=corr_drop_cols)
     x_cols = [c for c in x_cols if c not in corr_drop_cols]
     continuous = [c for c in continuous if c not in corr_drop_cols]
@@ -227,32 +228,40 @@ def dgp_acic_2018_df(acic_file, perc_train=None, n_train=None):
     df_train = df_train.drop(columns=['TE', 'Y0_true', 'Y1_true'])
     df_true = df.copy(deep=True)[train_idx:]
     df_assess = df_true.copy(deep=True).drop(columns=['TE', 'Y0_true', 'Y1_true'])
+    sample_nums = list(df_true.index)
     return df_train.reset_index(drop=True), df_assess.reset_index(drop=True), df_true.reset_index(drop=True), x_cols, \
-           binary, categorical, dummy_cols, categorical_to_dummy
+           binary, categorical, dummy_cols, categorical_to_dummy, sample_nums
 
 
 def clean_2018_covariates(df):
-    ordinal_unknowns = {'meduc': 9, 'fagecomb': 99, 'ufagecomb': 99, 'precare': 99, 'precare_rec': 5, 'uprevis': 99, 'previs_rec': 12,
-                        'wtgain': 99, 'wtgain_rec': 9, 'cig_1': 99, 'cig_2': 99, 'cig_3': 99, 'rf_ncesar': 99, 'apgar5': 99, 'apgar5r': 5,
-                        'estgest': 99, 'combgest': 99, 'gestrec10': 99, 'bwtr14': 14}
-    int_type = ['mager41', 'mager14', 'mager9', 'meduc', 'fagecomb', 'ufagecomb', 'precare', 'precare_rec', 'uprevis', 'previs_rec',
-               'wtgain', 'wtgain_rec', 'cig_1', 'cig_2', 'cig_3', 'rf_ncesar', 'apgar5', 'apgar5r', 'dplural', 'estgest', 'combgest',
-               'gestrec10', 'dbwt', 'bwtr14']
+    ordinal_unknowns = {'meduc': 9, 'precare': 99, 'uprevis': 99,
+                        'wtgain': 99, 'cig_1': 99, 'cig_2': 99, 'cig_3': 99,
+                        'rf_ncesar': 99, 'apgar5': 99,
+                        'estgest': 99, 'combgest': 99, 'bwtr14': 14}
+    int_type = ['mager41', 'meduc', 'precare', 'uprevis', 'wtgain', 'cig_1',
+                'cig_2', 'cig_3', 'rf_ncesar', 'apgar5', 'dplural', 'estgest',
+                'combgest', 'bwtr14']
     float_type = ['recwt']
 
     edited_cols = ['sample_id']
 
-    ordinal_unknowns_cols = [list(df.columns).index(i) for i in ordinal_unknowns.keys()]
-    binary = []
-    for k, v in ordinal_unknowns.items():
-        new_col = f'{k}_missing'
-        binary.append(new_col)
-        df.loc[:, new_col] = 0
-        df.loc[df[k] == v, new_col] = 1
-        df.loc[df[k] == v, k] = np.nan
-    df.iloc[:, ordinal_unknowns_cols] = IterativeImputer().fit_transform(df[df.columns.difference(binary)])[:, ordinal_unknowns_cols]
-    edited_cols += list(ordinal_unknowns.keys())
+    recode_drops = ['precare_rec', 'wtgain_rec', 'previs_rec',
+                    'cig_rec', 'mager14', 'mager9', 'apgar5r', 'gestrec10',
+                    'gestrec3', 'bwtr4', 'dbwt', 'rf_cesar']
 
+    df = df.drop(columns=recode_drops)
+
+    # ordinal_unknowns_cols = [list(df.columns).index(i) for i in ordinal_unknowns.keys()]
+    for k, v in ordinal_unknowns.items():
+        df = df.loc[~(df[k] == v)]
+        # new_col = f'{k}_missing'
+        # binary.append(new_col)
+        # df.loc[:, new_col] = 0
+        # df.loc[df[k] == v, new_col] = 1
+        # df.loc[df[k] == v, k] = np.nan
+    # df.iloc[:, ordinal_unknowns_cols] = IterativeImputer().fit_transform(df[df.columns.difference(binary)])[:, ordinal_unknowns_cols]
+    edited_cols += list(ordinal_unknowns.keys())
+    binary = []
     for c in df.columns:
         if len(df[c].unique()) == 2:
             df[c] = df[c].map(dict(zip(set(df[c].unique()), [0, 1])))